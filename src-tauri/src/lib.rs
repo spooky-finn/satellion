b36d88c16977037cc38097951b4e9c3b8e8d1db8
finn <loseev5@gmail.com>
2025-10-17 21:47:43 +0300

    Revert "feat: configure sqlite"
    This reverts commit 7e662afbf2f1f9af1dab4391966c2c33ad763c9e.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,25 +2,22 @@ mod app_state;
 mod commands;
 mod neutrino;
 
+use std::sync::Arc;
+
 use crate::neutrino::Neutrino;
-use sqlx::{Connection, SqliteConnection};
-use std::{fs::File, path::Path, sync::Arc};
 use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub async fn run() {
-    let db_conn = open_database().await;
-
+pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .manage(Arc::new(app_state::AppState::new()))
-        .manage(db_conn)
         .setup(move |app| {
             let state = app.state::<Arc<app_state::AppState>>();
 
             match Neutrino::connect_regtest() {
                 Ok(neutrino) => {
-                    let (node, event_rx) = (neutrino.node, neutrino.client.event_rx);
+                    let (node, client) = (neutrino.node, neutrino.client);
 
                     tauri::async_runtime::spawn(async move {
                         if let Err(e) = node.run().await {
@@ -29,7 +26,7 @@ pub async fn run() {
                     });
 
                     tauri::async_runtime::spawn(neutrino::handle_chain_updates(
-                        event_rx,
+                        client,
                         state.inner().clone(),
                     ));
                 }
@@ -42,15 +39,4 @@ pub async fn run() {
         .invoke_handler(tauri::generate_handler![commands::chain_status])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-async fn open_database() -> SqliteConnection {
-    let url = "sqlite://./blockchain.db";
-    if !Path::new("./blockchain.db").exists() {
-        File::create("./blockchain.db").expect("Failed to create database");
-    }
-
-    SqliteConnection::connect(url)
-        .await
-        .expect("Failed to connect to database")
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
-            satellion_lib::run().await;
-        });
+    satellion_lib::run()
 }
