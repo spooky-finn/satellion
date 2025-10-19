af2d9cf590a3ab5d39853937aeeff4b9f761cb13
finn <loseev5@gmail.com>
2025-10-19 17:09:40 +0300

    feat: mnemonic generator

--- src-tauri/src/mnemonic.rs
+++ src-tauri/src/mnemonic.rs
@@ -1,5 +1,16 @@
+use rand::Rng;
+
 const WORDLIST: &str = include_str!("../wordlist/english.txt");
 
-pub fn get_wordlist() -> Vec<&'static str> {
+fn get_wordlist() -> Vec<&'static str> {
     WORDLIST.lines().map(|line| line.trim()).collect()
+}
+
+pub fn generate_mnemonic(word_count: usize) -> Vec<&'static str> {
+    let words = get_wordlist();
+    let mut rng = rand::rng();
+
+    (0..word_count)
+        .map(|_| words[rng.random_range(0..words.len())])
+        .collect()
 }
