496dcf19dcbd22d1a2a7f2c8a17103675fc82881
Ivan Loseev <loseev5@gmail.com>
2026-04-24 21:51:47 +0300

    chore: rename files

--- src-tauri/src/mnemonic.rs
+++ src-tauri/src/mnemonic.rs
@@ -1,15 +1,15 @@
 use aes_gcm::aead::OsRng;
-use alloy_signer_local::coins_bip39::{self, English};
+use alloy_signer_local::coins_bip39::{English, Mnemonic};
 
 pub fn new() -> Result<String, String> {
     let mut rng = OsRng;
-    let mnemnonic = coins_bip39::Mnemonic::<English>::new_with_count(&mut rng, 12)
+    let mnemnonic = Mnemonic::<English>::new_with_count(&mut rng, 12)
         .map_err(|e| format!("failed to generate mnemonic {e}"))?;
     Ok(mnemnonic.to_phrase())
 }
 
 pub fn validate(mnemonic: &str) -> Result<bool, String> {
-    coins_bip39::Mnemonic::<English>::new_from_phrase(mnemonic)
+    Mnemonic::<English>::new_from_phrase(mnemonic)
         .map(|_| true)
         .map_err(|e| format!("Invalid mnemonic: {e}"))
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,6 @@ export const useNavigate = useNavigateReactRouter
 export const route = {
   unlock_wallet: '/',
   create_wallet: '/create_wallet',
-  // chain specific routes
   ethereum: '/wallet/ethereum',
   bitcoin: '/wallet/bitcoin',
 } as const
